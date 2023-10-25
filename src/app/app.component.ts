83a5ae86d14fd9d55f6ecb38a8ba40a00f7971cc
Rafael Rodrigues <rafael.raraujo86@gmail.com>
2023-10-25 16:33:51 -0300

    Inicializar projeto

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,60 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'pw';
+  length = 0;
+  includeLetters = false;
+  includeNumbers = false;
+  includeSymbols = false;
+  password = '';
+
+  // ? This above (line 9) is the same thing as this
+
+  // constructor() {
+  //   this.password = '';
+  // }
+
+  onChangeUseLetters() {
+    this.includeLetters = !this.includeLetters;
+  }
+
+  onChangeUseNumbers() {
+    this.includeNumbers = !this.includeNumbers;
+  }
+
+  onChangeUseSymbols() {
+    this.includeSymbols = !this.includeSymbols;
+  }
+
+  onChangeLength(event: Event) {
+    const target = event.target as HTMLInputElement;
+    const parsedValue = +target.value;
+    if (!isNaN(parsedValue)) {
+      this.length = parsedValue;
+    }
+    console.log(this.length);
+  }
+
+  onButtonClick() {
+    const numbers = '1234567890';
+    const letters = 'abcdefjghiklmnopqrstuvwxyz';
+    const symbols = '!@#$%¨&*()';
+
+    let validChars = '';
+
+    if (this.includeLetters) validChars += letters;
+    if (this.includeNumbers) validChars += numbers;
+    if (this.includeSymbols) validChars += symbols;
+
+    let generatedPassword = '';
+
+    for (let i = 0; i < this.length; i++) {
+      const index = Math.floor(Math.random() * validChars.length);
+      generatedPassword += validChars[index];
+    }
+
+    this.password = generatedPassword;
+  }
 }
